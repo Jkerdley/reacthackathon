1aecc212942b758f60e12e52e369c8566e09a85a
Jkerdley <jk-erley@yandex.ru>
2025-01-18 00:47:55 +0700

    Добавил новые зависимости

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+	content: [
+		'./index.html',
+		'./src/**/*.{js,jsx,ts,tsx}', // Убедитесь, что вы добавили все необходимые расширения
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};

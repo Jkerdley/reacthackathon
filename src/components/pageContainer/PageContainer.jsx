516e8d272b67f2830881e5543095eac4ccd8268b
Andrey <pachalia2009@yandex.com>
2025-01-18 14:19:44 +0300

    Добавил в ContentContainer типизацию PropTypes.

--- src/components/pageContainer/PageContainer.jsx
+++ src/components/pageContainer/PageContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const PageContainer = ({ children }) => {
 	return (
@@ -7,3 +8,7 @@ export const PageContainer = ({ children }) => {
 		</div>
 	);
 };
+
+PageContainer.propTypes = {
+	children: PropTypes.node.isRequired
+}

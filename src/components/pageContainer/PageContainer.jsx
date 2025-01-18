0c68b35369be779a3742610d80e79882ef403f34
Jkerdley <jk-erley@yandex.ru>
2025-01-18 19:31:27 +0700

    Добавил карточки на страницу команды. Сделал их анимированными при наведении.

--- src/components/pageContainer/PageContainer.jsx
+++ src/components/pageContainer/PageContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const PageContainer = ({ children }) => {
 	return (
-		<div className="flex flex-[10] w-full h-full items-center justify-center bg-[#813177] rounded-2xl p-2 gap-2">
+		<div className="flex flex-wrap flex-[10] w-full h-full items-center justify-center bg-[#33394D]/35 rounded-2xl p-3 gap-3">
 			{children}
 		</div>
 	);

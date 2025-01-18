dac66497f3e66709a81e3a042108c848088f2d46
Jkerdley <jk-erley@yandex.ru>
2025-01-18 23:42:06 +0700

    Поправил app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { Badge } from './components/badge/badge';
+import { BreadCrumbs } from './components/breadcrumbs';
+import NavBar from './components/navigation/NavBar';
+import { ContentContainer } from './components/content/ContentContainer';
 
 function App() {
 	return (
 		<div
 			name="main-container"
-			className="flex flex-col bg-[#14141B] w-full h-auto items-start overflow-hidden m-20 p-10"
+			className="flex flex-col bg-[#14141B] w-full h-auto items-start overflow-hidden"
 		>
-			<Badge content="Badge" color="yellow" />
+			<NavBar />
+			<BreadCrumbs />
+			<ContentContainer></ContentContainer>
 		</div>
 	);
 }

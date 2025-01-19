a0efc3e7564d4386250fc39969bea63093217268
erdeni <erdeni@bk.ru>
2025-01-19 21:26:48 +0800

    Удален отладочный код

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -4,7 +4,7 @@ import { OfferForm, TeamFan } from './components';
 export const Offer = () => {
 	return (
 		<div className="flex flex-col justify-center items-center w-full h-full border">
-			<div className="w-full h-1/3 p-5 border">
+			<div className="w-full h-1/3 p-5">
 				<TeamFan />
 			</div>
 			<div className="w-full h-3/4">

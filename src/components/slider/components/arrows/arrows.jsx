5df185b8adaa2722a90f339e36e742dff0591fc6
erdeni <erdeni@bk.ru>
2025-01-20 03:15:29 +0800

    Изменен стилизация кнопок и заголовка

--- src/components/slider/components/arrows/arrows.jsx
+++ src/components/slider/components/arrows/arrows.jsx
@@ -6,14 +6,14 @@ import rightArrow from '../../assets/rightArrow.svg';
 export const Arrows = ({ changeSlide }) => {
 	return (
 		<div className="flex justify-between items-center h-full w-full absolute z-10 px-2">
-			<div className="backdrop-opacity-20 backdrop-invert p-2 rounded-full">
+			<div className="backdrop-opacity-40 backdrop-invert p-2 rounded-full">
 				<img
 					className="h-[20px] w-[20px] hover:cursor-pointer"
 					src={leftArrow}
 					onClick={() => changeSlide(-1)}
 				/>
 			</div>
-			<div className="backdrop-opacity-20 backdrop-invert p-2 rounded-full">
+			<div className="backdrop-opacity-40 backdrop-invert p-2 rounded-full">
 				<img
 					className="h-[20px] w-[20px] hover:cursor-pointer"
 					src={rightArrow}
--- src/components/slider/components/slide/slide.jsx
+++ src/components/slider/components/slide/slide.jsx
@@ -7,7 +7,7 @@ export const Slide = ({ imageUrl, title = '', url }) => {
 	return (
 		<div className="flex-[1_0_100%] relative">
 			<img src={imageUrl} alt={title} className="flex mt-auto mx-auto w-full h-full object-contain" />
-			<span className="absolute left-1/2 -translate-x-1/2 bottom-4 backdrop-opacity-20 backdrop-invert px-2 rounded-md">
+			<span className="absolute left-1/2 -translate-x-1/2 bottom-4 backdrop-opacity-40 backdrop-invert px-2 rounded-md">
 				{url ? (
 					<a href={url} className="text-white hover:underline cursor-pointer">
 						{title}

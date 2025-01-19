a4ca8b1541c0d484a310aeac388f8d45d9d41f26
erdeni <erdeni@bk.ru>
2025-01-19 22:37:54 +0800

    Исправлены ошибки стиля

--- src/components/offer/components/offerForm/offerForm.jsx
+++ src/components/offer/components/offerForm/offerForm.jsx
@@ -1,25 +1,28 @@
 import { React } from 'react';
 import { Input } from '../../../input';
 import { Button } from '../../../button';
+import { BUTTON_CORNER_TYPE } from '../../../button/constants';
 
 export const OfferForm = () => {
 	return (
 		<>
-			<form className="text-center pt-3 m-5">
+			<form className="flex flex-col gap-5 text-center pt-3 m-5">
 				<h1 className="text-5xl font-bold">
 					Frontend-team
 					<br /> к Вашим услугам!
-				</h1>{' '}
-				<Input className="mt-10 text-lg" height="48px" placeholder="e-mail/Телефон" />
-				<Button
-					className="mt-10"
-					title="ЗАКАЗАТЬ САЙТ"
-					color="#FFFFFF"
-					height="48px"
-					backgroundColor="#D03982"
-					cornerType="rounded"
-				/>
-				<a className="mt-5 block" href="#">
+				</h1>
+				<Input className="text-lg" height="48px" placeholder="e-mail/Телефон" />
+				<div>
+					<Button
+						title="ЗАКАЗАТЬ САЙТ"
+						color="#FFFFFF"
+						height="48px"
+						backgroundColor="#D03982"
+						cornerType={BUTTON_CORNER_TYPE.ROUNDED}
+					/>
+				</div>
+
+				<a className="block" href="#">
 					Публичная оферта
 				</a>
 			</form>

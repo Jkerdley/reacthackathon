1d3711f54a3b4502937df11079b2ed30340dbec6
Jkerdley <jk-erley@yandex.ru>
2025-01-20 02:18:11 +0700

    Доработал хлебные крошки

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import NavBar from '../navigation/NavBar.jsx';
 import { BreadCrumbs } from '../breadcrumbs/index.js';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export const Layout = () => {
+	const location = useLocation();
 
-	const breadcrumbs = [
-		{ label: 'Главная', path: '/reacthackathon/' },
-		{ label: 'Команда', path: '/reacthackathon/teammates' },
-		{ label: 'Избранное', path: '/reacthackathon/favorite' },
-	];
+	const breadcrumbs = () => {
+		const pathnames = location.pathname.split('/').filter((x) => x);
+		const paths = {
+			reacthackathon: 'Главная',
+			teammates: 'Команда',
+			teammate: 'Teammate',
+			favorite: 'Избранное',
+		};
+
+		return pathnames.map((path, index) => {
+			const routePath = `/${pathnames.slice(0, index + 1).join('/')}`;
+			return {
+				label: paths[path] || path,
+				path: routePath,
+			};
+		});
+	};
+
 	return (
 		<div className="flex flex-col bg-[#14141B] w-full h-auto items-start overflow-hidden">
 			<NavBar />
-			<BreadCrumbs breadcrumbs={breadcrumbs} />
+			<BreadCrumbs breadcrumbs={breadcrumbs()} />
 			<div className="flex flex-col flex-[9] w-[100%] bg-[#14141B] items-center justify-between pr-4 pb-4 pl-4">
 				<div className="flex flex-[12] w-full h-full">
 					<Outlet />
